3c934809176eac8ca6287cfb2a8ed9e38f212f2e
kierhb <kierbobila@gmail.com>
2022-12-12 17:24:54 +0800

    Finalize codes

--- index.js
+++ index.js
@@ -6,26 +6,43 @@ const massEl = document.querySelector("#mass-el")
 
 
 inputBtn.addEventListener("click", function() {
-    let valueToConvert = inputEl.value
-
-})
-
-inputBtn.addEventListener("click", function() {
     const valueToConvert = inputEl.value
-    
-    // Length
-    const metersToFeet = valueToConvert * 3.281
-    const feetToMeters = valueToConvert / 3.281
-    lengthEl.textContent = `${valueToConvert} meters = ${metersToFeet.toFixed(3)} feet | ${valueToConvert} feet = ${feetToMeters.toFixed(3)} meters`
-    
-    // Volume
-    const litersToGallons = valueToConvert * 0.264
-    const gallonsToLiters = valueToConvert / 0.264
-    volumeEl.textContent = `${valueToConvert} liters = ${litersToGallons.toFixed(3)} gallons | ${valueToConvert} gallons = ${gallonsToLiters.toFixed(3)} liters`
-    
-    // Mass
-    const kilosToPounds = valueToConvert * 2.205
-    const poundsToKilos = valueToConvert / 2.205
-    massEl.textContent = `${valueToConvert} kilos = ${kilosToPounds.toFixed(3)} pounds | ${valueToConvert} pounds = ${poundsToKilos.toFixed(3)} kilos`
 
+    if (Number(valueToConvert)) {    
+        // Length
+        lengthConversion(valueToConvert)
+        // Volume
+        volumeConversion(valueToConvert)
+        // Mass
+        massConversion(valueToConvert)
+    } else {
+        const errorMessage = `<p id="error-message">Invalid input</p>`
+        lengthEl.innerHTML = errorMessage
+        volumeEl.innerHTML = errorMessage
+        massEl.innerHTML = errorMessage
+    }
 })
+
+// Length Conversion
+function lengthConversion(input) {
+    const convertInput = Math.abs(input)
+    const metersToFeet = convertInput * 3.281
+    const feetToMeters = convertInput / 3.281
+    lengthEl.textContent = `${convertInput} meters = ${metersToFeet.toFixed(3)} feet | ${convertInput} feet = ${feetToMeters.toFixed(3)} meters`
+}
+
+// Volume Conversion
+function volumeConversion(input) {
+    const convertInput = Math.abs(input)
+    const litersToGallons = convertInput * 0.264
+    const gallonsToLiters = convertInput / 0.264
+    volumeEl.textContent = `${convertInput} liters = ${litersToGallons.toFixed(3)} gallons | ${convertInput} gallons = ${gallonsToLiters.toFixed(3)} liters`
+}
+
+// Mass Conversion
+function massConversion(input) {
+    const convertInput = Math.abs(input)
+    const kilosToPounds = convertInput * 2.205
+    const poundsToKilos = convertInput / 2.205
+    massEl.textContent = `${convertInput} kilos = ${kilosToPounds.toFixed(3)} pounds | ${convertInput} pounds = ${poundsToKilos.toFixed(3)} kilos`
+}
